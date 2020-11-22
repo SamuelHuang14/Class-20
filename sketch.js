202eb17d95fd379cf1df9fa04de50de99bfcdb35
SamuelHuang14 <73797762+SamuelHuang14@users.noreply.github.com>
2020-11-22 15:35:06 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var fixedRect
+var movingRect
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(200, 200, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 200, 80, 30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+}
+
+function draw() {
+  background(0);
+  movingRect.y = World.mouseY;
+  movingRect.x = World.mouseX;
+  if(movingRect.x-fixedRect.x<movingRect.width/2+fixedRect.width/2
+    && fixedRect.x-movingRect.x<movingRect.width/2+fixedRect.width/2
+    && movingRect.y-fixedRect.y<movingRect.height/2+fixedRect.height/2
+    && fixedRect.y-movingRect.y<movingRect.height/2+fixedRect.height/2){
+    movingRect.shapeColor="red";
+    fixedRect.shapeColor="red";
+    
+  }
+  else{
+    movingRect.shapeColor="green";
+    fixedRect.shapeColor="green";
+  }
+  console.log(movingRect.x-fixedRect.x);
+  drawSprites();
+}
